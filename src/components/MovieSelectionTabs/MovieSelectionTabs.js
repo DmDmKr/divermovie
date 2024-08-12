b9854d0cdd62fa7ac9dd4632bc6f9f89b6d971f9
DmDmKr <deimon.blackreign@gmail.com>
2024-08-12 19:44:56 +0200

    implement button selection visibility

--- src/components/MovieSelectionTabs/MovieSelectionTabs.js
+++ src/components/MovieSelectionTabs/MovieSelectionTabs.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import { useState } from 'react'
 import './MovieSelectionTabs.css'
 
 const MovieSelectionTabs = () => {
   const location = useLocation()
+  const [selectedButtonId, setSelectedButtonId] = useState('diverButton')
   const currentPath = location.pathname
 
   return (
     <div>
       <ul className="MovieSelectionTabs">
-        <li>
+        <button
+          id="diverButton"
+          onClick={() => setSelectedButtonId('diverButton')}
+          className={selectedButtonId === 'diverButton' ? 'selected' : ''}
+        >
           <Link to={`${currentPath}?movie=diver`}>Ныряльщик</Link>
-        </li>
-        <li>
+        </button>
+        <button
+          id="drunkNotesButton"
+          onClick={() => setSelectedButtonId('drunkNotesButton')}
+          className={selectedButtonId === 'drunkNotesButton' ? 'selected' : ''}
+        >
           <Link to={`${currentPath}?movie=drunk_notes`}>Записки по пьяни</Link>
-        </li>
+        </button>
       </ul>
     </div>
   )
